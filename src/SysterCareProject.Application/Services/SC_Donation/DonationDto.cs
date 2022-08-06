a800fe4ad11e92ffdf088f8146cb02bac245e5a0
Nyarai Nedziwe <nyarain@boxfusion.co.za>
2022-08-06 15:55:52 +0200

    Donation

---
+++ src/SysterCareProject.Application/Services/SC_Donation/DonationDto.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SysterCareProject.Domain.RefList.Gender_RefList;
+
+namespace SysterCareProject.Services.SC_Donation
+{
+    public class DonationDto:EntityDto<Guid>
+    {
+        public  Ref_List DonationType { get; set; }
+    }
+}
